e37c23d764a89d143ba5b454721cf15a7cbe5c0b
natecheadle_school <natecheadle@users.noreply.github.com>
2021-11-01 21:16:34 -0700

    Updated for Support for Mac

--- main.cpp
+++ main.cpp
@@ -9,16 +9,23 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 int main(int argc, char* argv[])
 {
-    glfwInit();
+    if(!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
